14a0f0b62aba01a7079c483ab804da336f2b55de
abhishek-incepted <abhishek.incepted@gmail.com>
2016-07-28 13:08:03 +0530

    .bat changes

--- src/main/deploy/scripts/start.bat
+++ src/main/deploy/scripts/start.bat
@@ -1,5 +1,5 @@
 echo 'starting JMX SERVER'
 
-java -XX:MaxPermSize=512m -XX:-HeapDumpOnOutOfMemoryError -XX:-PrintGCDetails -XX:+PrintGCTimeStamps -Xms1024m -Xmx4096m -Dindices.jmx.server.host=localhost -Dindices.jmx.server.port=50421 -Dapplication.properties=jmx.properties -cp lib\* JMXMonitoringStartup
+java -XX:MaxPermSize=512m -XX:-HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jmx-monitoring -XX:-PrintGCDetails -XX:+PrintGCTimeStamps -Xms1024m -Xmx4096m -Dmonitoring.jmx.server.host=localhost -Dmonitoring.jmx.server.port=50421 -Dapplication.properties=jmx.properties -cp lib\* JMXMonitoringStartup
 
 echo 'started JMX SERVER'
