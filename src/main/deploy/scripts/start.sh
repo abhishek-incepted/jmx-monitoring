cb8a1b11ab8a5d32a235ea209fea6beb74808d51
abhishek-incepted <abhishek.incepted@gmail.com>
2016-07-28 13:09:30 +0530

    run shell script updated

--- src/main/deploy/scripts/start.sh
+++ src/main/deploy/scripts/start.sh
@@ -1,5 +1,5 @@
 echo 'starting JMX SERVER'
 
-nohup /etc/alternatives/java_sdk/bin/java -XX:MaxPermSize=512m -XX:-HeapDumpOnOutOfMemoryError -XX:-PrintGCDetails -XX:+PrintGCTimeStamps -Xms1024m -Xmx2048m -Dindices.jmx.server.host=localhost -Dindices.jmx.server.port=50420 -Dapplication.properties=jmx.properties -cp lib/jmx-monitoring.jar:lib/* JMXMonitoringStartup > analytics_log.out &
+nohup /etc/alternatives/java_sdk/bin/java -XX:MaxPermSize=512m -XX:-HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jmx-monitoring -XX:-PrintGCDetails -XX:+PrintGCTimeStamps -Xms1024m -Xmx2048m -Dmonitoring.jmx.server.host=localhost -Dmonitoring.jmx.server.port=50420 -Dapplication.properties=jmx.properties -cp lib/jmx-monitoring.jar:lib/* JMXMonitoringStartup > jmx_monitoring.log &
 
 echo 'started JMX SERVER'
