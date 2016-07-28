f7a7586180a4b77b8950b0cab0390bbf8be0b6aa
abhishek-incepted <abhishek.incepted@gmail.com>
2016-07-28 13:15:07 +0530

    Main file Launcher updated in bat file

--- src/main/deploy/scripts/start.bat
+++ src/main/deploy/scripts/start.bat
@@ -1,5 +1,5 @@
 echo 'starting JMX SERVER'
 
-java -XX:MaxPermSize=512m -XX:-HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jmx-monitoring -XX:-PrintGCDetails -XX:+PrintGCTimeStamps -Xms1024m -Xmx4096m -Dmonitoring.jmx.server.host=localhost -Dmonitoring.jmx.server.port=50421 -Dapplication.properties=jmx.properties -cp lib\* JMXMonitoringStartup
+java -XX:MaxPermSize=512m -XX:-HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/var/log/jmx-monitoring -XX:-PrintGCDetails -XX:+PrintGCTimeStamps -Xms1024m -Xmx4096m -Dmonitoring.jmx.server.host=localhost -Dmonitoring.jmx.server.port=50421 -Dapplication.properties=jmx.properties -cp lib\* com.ndyatech.jmx.monitoring.rest.services.JMXMonitoringStartup
 
 echo 'started JMX SERVER'
